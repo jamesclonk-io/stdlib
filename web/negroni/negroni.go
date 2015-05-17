f405745d33d817a75c4fe4ba73438531c0e5685d
JamesClonk <jamesclonk@jamesclonk.ch>
2015-05-17 11:28:10 +0200

    bugfix

--- web/negroni/negroni.go
+++ web/negroni/negroni.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	classico "github.com/codegangsta/negroni"
-	"github.com/jamesclonk-io/stdlib/web"
 )
 
 type Negroni struct {
@@ -13,8 +12,8 @@ type Negroni struct {
 
 func Sbagliato() *Negroni {
 	n := classico.New()
-	n.Use(web.NewRecovery())
-	n.Use(web.NewLogger())
+	n.Use(NewRecovery())
+	n.Use(NewLogger())
 	n.Use(classico.NewStatic(http.Dir("public")))
 	return &Negroni{n}
 }
--- web/negroni/negroni_test.go
+++ web/negroni/negroni_test.go
@@ -10,7 +10,7 @@ import (
 func Test_Negroni_Sbagliato(t *testing.T) {
 	n := Sbagliato()
 	assert.Equal(t, 3, len(n.Handlers()))
-	assert.Equal(t, "*web.Recovery", reflect.TypeOf(n.Handlers()[0]).String())
-	assert.Equal(t, "*web.Logger", reflect.TypeOf(n.Handlers()[1]).String())
+	assert.Equal(t, "*negroni.Recovery", reflect.TypeOf(n.Handlers()[0]).String())
+	assert.Equal(t, "*negroni.Logger", reflect.TypeOf(n.Handlers()[1]).String())
 	assert.Equal(t, "*negroni.Static", reflect.TypeOf(n.Handlers()[2]).String())
 }
