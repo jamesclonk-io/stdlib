eda730630857573310ab5e6a76ade809579ef4eb
JamesClonk <jamesclonk@jamesclonk.ch>
2015-05-31 10:55:43 +0200

    add gzip compression

--- web/negroni/negroni.go
+++ web/negroni/negroni.go
@@ -4,16 +4,30 @@ import (
 	"net/http"
 
 	classico "github.com/codegangsta/negroni"
+	"github.com/phyber/negroni-gzip/gzip"
 )
 
 type Negroni struct {
 	*classico.Negroni
 }
 
-func Sbagliato() *Negroni {
+func Classico() *Negroni {
 	n := classico.New()
+	return &Negroni{n}
+}
+
+func Sbagliato() *Negroni {
+	n := Classico()
+	n.Use(NewRecovery())
+	n.Use(NewLogger())
+	n.Use(gzip.Gzip(gzip.DefaultCompression))
+	n.Use(classico.NewStatic(http.Dir("public")))
+	return &Negroni{n}
+}
+
+func (n *Negroni) Mescolare() *Negroni {
 	n.Use(NewRecovery())
 	n.Use(NewLogger())
 	n.Use(classico.NewStatic(http.Dir("public")))
-	return &Negroni{n}
+	return n
 }
